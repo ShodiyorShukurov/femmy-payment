e3b4f3912667e7807b5a96852046131eba3ca7d0
ShodiyorShukurov <shodiyorshukurov07@gmail.com>
2025-04-21 19:20:53 +0500

    premium function add

--- src/components/FooterButton.jsx
+++ src/components/FooterButton.jsx
@@ -1,7 +1,15 @@
 function FooterButton() {
+  function closeWebView() {
+    if (window.CloseWebViewChannel) {
+      CloseWebViewChannel.postMessage("premium");
+    } else {
+      window.history.back(); 
+    }
+  }
+
     return (
       <div className="p-4 container">
-        <button className="w-full bg-[#EB2D69] text-white text-[16px] leading-[24px] py-4 rounded-[12px] font-bold">
+        <button onClick={closeWebView} className="w-full bg-[#EB2D69] text-white text-[16px] leading-[24px] py-4 rounded-[12px] font-bold">
           Premiumga o‘tish
         </button>
       </div>
