c107a18eedd5707a621141499c7bd26c9d6ffd53
ShodiyorShukurov <shodiyorshukurov07@gmail.com>
2025-04-24 20:22:30 +0500

    function fixed

--- src/components/FooterButton.jsx
+++ src/components/FooterButton.jsx
@@ -1,19 +1,19 @@
 import { NavLink } from 'react-router-dom';
 
 function FooterButton() {
-  function closeWebView() {
-    if (window.CloseWebViewChannel) {
-      CloseWebViewChannel.postMessage('close');
-    } else {
-      window.history.back();
-    }
-  }
+  // function closeWebView() {
+  //   if (window.CloseWebViewChannel) {
+  //     CloseWebViewChannel.postMessage('close');
+  //   } else {
+  //     window.history.back();
+  //   }
+  // }
 
   return (
     <div className="sticky bottom-0 px-4 pt-4 pb-[max(16px,env(safe-area-inset-bottom))] container">
       <NavLink
         to="/payment"
-        onClick={closeWebView}
+        // onClick={closeWebView}
         className="w-full block text-center bg-[#EB2D69] text-white text-[16px] leading-[24px] py-4 rounded-[12px] font-bold"
       >
         Premiumga o‘tish
